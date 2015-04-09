f343b6557d79f9c4a901a6128ccdee4b6671f458
Daedalus2051 <sixstringtrebleterror@gmail.com>
2015-04-09 11:48:03 -0400

    Update commit
    Removed unused "using" statements

--- PlowTruck/PlowTruckCore.cs
+++ PlowTruck/PlowTruckCore.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace PlowTruck
