bcf926067f072bee20b6bed6de4126656113433a
Russell Hoyer <sixstringtrebleterror@gmail.com>
2016-04-29 13:38:31 -0400

    Config
    Started writing the config class elements. Also added Core.cs to get
    that started.

--- PlowTruck/Configuration.cs
+++ PlowTruck/Configuration.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using Logger;
 
 namespace PlowTruck
 {
     class Configuration
     {
+        private LogWriter ConfigLogger = new LogWriter(Environment.CurrentDirectory, "PlowTruck");
 
+        private string _plowPath;
+        private string _destPath;
+        private Dictionary<string, string> _extLookup;
+
+        public string PlowPath
+        {
+            get { return _plowPath; }
+            set {
+                if (Directory.Exists(value))
+                    _plowPath = value;
+                else
+                    throw new DirectoryNotFoundException();
+            }
+        }
+        public string DestinationPath
+        {
+            get { return _destPath; }
+            set {
+                if (Directory.Exists(value))
+                    _destPath = value;
+                else
+                    throw new DirectoryNotFoundException();
+            }
+        }
+        public Dictionary<string, string> ExtensionLookup
+        {
+            get { return _extLookup; }
+        }
 
         public Configuration()
+        {
+            ConfigLogger.WriteLog(LogWriter.LOG_TYPE.VERBOSE, "", "");
+        }
+
+        // Thinking this method will run all of the methods necessary to read the XML file
+        // and populate the internal variables with the information needed
+        public void LoadConfig()
         {
         }
     }
---
+++ PlowTruck/Core.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlowTruck
+{
+    class Core
+    {
+        public bool Verbose=false;
+
+        public Core()
+        {
+        }
+    }
+}
