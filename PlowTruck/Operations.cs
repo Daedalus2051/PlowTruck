c2a02f1e2d69c50b2d938147f09ecfee61ad6499
Daedalus2051 <Daedalus2051@TALYN>
2016-08-12 01:19:31 -0400

    Added Plow methods and EmployAction to execute the appropriate action on each file

--- PlowTruck/Operations.cs
+++ PlowTruck/Operations.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +23,7 @@ namespace PlowTruck
         }
         #endregion
 
+        #region Methods
         // Scan
         public void Scan(Configuration conf)
         {
@@ -51,8 +54,84 @@ namespace PlowTruck
         }
 
         // Plow (scan first)
+        public void Plow(string PlowPath)
+        {
+
+        }
 
         // Plow (plow based on scan results)
+        public void Plow(Results FolderScanResults)
+        {
+
+        }
+
+        private void EmployAction(string FilePath, string Action, string ActionValue)
+        {
+            switch(Action.ToLower())
+            {
+                case "move":
+                    try
+                    {
+                        File.Move(FilePath, ActionValue); // Might need to add logic to get the path to the file that we're plowing
+                    }
+                    catch(Exception MoveErr)
+                    {
+                        // TODO: Log error?
+                    }
+                    break;
+
+                case "delete":
+                    try
+                    {
+                        File.Delete(FilePath);
+                    }
+                    catch(Exception DeleteErr)
+                    {
+                        // TODO: Log error?
+                    }
+                    break;
+
+                case "exclude":
+                    Console.WriteLine($"Excluding file {FilePath}");
+                    return;
+
+                case "archive":
+                    // Search for a 7-zip executable
+                    if (File.Exists(@"C:\Program Files\7-Zip\7z.exe"))
+                    {
+                        // If we found the 7-zip executable then use it
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.CreateNoWindow = true;
+                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        startInfo.FileName = @"C:\Program Files\7-Zip\7z.exe";
+                        startInfo.Arguments = "a " + (ActionValue + ".7z") + " " + FilePath;
+                        try
+                        {
+                            using (Process proc_7z = Process.Start(startInfo))
+                            {
+                                proc_7z.WaitForExit();
+                            }
+                        }
+                        catch (Exception err)
+                        {
+                            // TODO: Log error?
+                            /*
+                            plowLog.WriteLog(LogWriter.LOG_TYPE.ERROR, "Error in archive step: " + err.Message,
+                                this.GetType().Name + "." + MethodBase.GetCurrentMethod().Name);*/
+                        }
+                    }
+                    else
+                    {
+                        // TODO: Implement the .NET archive default
+                        Console.WriteLine("7-zip not found in Program Files and Dot Net archive function not yet implemented.");
+                    }
+                    break;
+
+                default:
+                    throw new Exception($"Action {Action} is unknown or not supported.");
+            }
+        }
+        #endregion
 
         #region Classes
         public class Results
@@ -67,7 +146,7 @@ namespace PlowTruck
                 _columns[1] = new DataColumn("Extension");
                 _columns[2] = new DataColumn("Action");
                 _columns[3] = new DataColumn("ActionValue");
-                _columns[4] = new DataColumn("Matched");
+                _columns[4] = new DataColumn("Matched", typeof(bool));
 
                 foreach(DataColumn col in _columns)
                 {
